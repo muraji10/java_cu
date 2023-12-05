e8efc6264df72c1562b4cae7c459c532f38b1c67
mura1 <mura1@DESKTOP-RLSG1V2>
2023-12-05 21:19:19 +0900

    課題1.3　自己紹介文

---
+++ HelloWorld_FamilynameFirstname/src/sample/SelfIntroduction.java
@@ -0,0 +1,24 @@
+package sample;
+
+public class SelfIntroduction {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+		// 氏名
+		String name = "村治 優花";
+		// 年齢
+		int age = 25;
+		// 趣味
+		String hobby = "雑貨めぐり";
+		
+		/*
+		* 自己紹介文を出力する処理
+		*/
+
+		System.out.println("私の名前は" + name + "です。");
+		System.out.println("年齢は" + age + "歳です。");
+		System.out.println("趣味は" + hobby + "です。");
+	}
+
+}
